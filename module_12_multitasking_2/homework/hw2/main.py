cda8913b66cc070f7f0ddb595f4b7b14529093d1
Семён Арефьев <semioan2000@mail.ru>
2024-09-26 15:51:05 +0700

    Модуль выполнен.

--- module_12_multitasking_2/homework/hw2/main.py
+++ module_12_multitasking_2/homework/hw2/main.py
@@ -1,10 +1,23 @@
+import psutil
+import os
+
+
 def process_count(username: str) -> int:
-    # количество процессов, запущенных из-под
-    # текущего пользователя username
-    pass
+    current_user = os.getlogin()
+    processes = psutil.process_iter()
+    user_processes = [p for p in processes if p.username() == username]
+    return len(user_processes)
+
 
 
 def total_memory_usage(root_pid: int) -> float:
-    # суммарное потребление памяти древа процессов
-    # с корнем root_pid в процентах
-    pass
+    root_process = psutil.Process(root_pid)
+    all_processes = [root_process] + root_process.children(recursive=True)
+    total_usage = sum(proc.memory_percent() for proc in all_processes)
+
+    return total_usage
+
+
+if __name__ == "__main__":
+    print(process_count('simon'))
+    print(total_memory_usage(14571))
