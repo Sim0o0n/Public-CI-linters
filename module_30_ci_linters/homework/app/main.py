c90fcbb497ae1acde914389ebfb6467628c025e0
Simon <135053309+Sim0o0n@users.noreply.github.com>
2025-03-18 15:25:42 +0700

    Update main.py

--- module_30_ci_linters/homework/app/main.py
+++ module_30_ci_linters/homework/app/main.py
@@ -1,7 +1,8 @@
 import asyncio
 from fastapi import FastAPI, Depends, HTTPException, Request
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from .models import Table1, Table2, Base
 from sqlalchemy.future import select
 from sqlalchemy import desc, asc
