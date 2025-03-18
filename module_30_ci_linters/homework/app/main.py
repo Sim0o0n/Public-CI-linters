08707b43282df3b789756fa547af70d7c0f114aa
Simon <135053309+Sim0o0n@users.noreply.github.com>
2025-03-18 15:45:18 +0700

    Update main.py

--- module_30_ci_linters/homework/app/main.py
+++ module_30_ci_linters/homework/app/main.py
@@ -8,6 +8,7 @@ from sqlalchemy import desc, asc
 from pydantic import ValidationError
 from fastapi.responses import JSONResponse
 from .schemas import CookBook
+from typing import AsyncGenerator
 
 # Создание экземпляра FastAPI
 app = FastAPI()
@@ -17,12 +18,12 @@ database_url = "sqlite+aiosqlite:///cookbook.db"
 # Создание асинхронного движка базы данных
 engine = create_async_engine(database_url, echo=True)
 # Создание сессии для работы с базой данных
-async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 # Базовый класс для объявляемых моделей
 Base = declarative_base()
 
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     """
     Создает асинхронную сессию базы данных.
 
