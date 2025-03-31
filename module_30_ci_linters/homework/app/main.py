c7d26eb406dd5fe8aac5d3d09b439f159ad60d1d
Simon <135053309+Sim0o0n@users.noreply.github.com>
2025-03-31 15:15:06 +0700

    Update main.py

--- module_30_ci_linters/homework/app/main.py
+++ module_30_ci_linters/homework/app/main.py
@@ -7,7 +7,6 @@ from sqlalchemy.future import select
 from sqlalchemy import asc, desc
 from pydantic import ValidationError
 from typing import AsyncGenerator
-
 from .models import Base, Table1, Table2
 from .schemas import CookBook
 
