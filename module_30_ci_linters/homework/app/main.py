4c2b30c48b25ab9e9a703dced89eec52d53bd2cb
Simon <135053309+Sim0o0n@users.noreply.github.com>
2025-03-31 16:44:33 +0700

    Update main.py

--- module_30_ci_linters/homework/app/main.py
+++ module_30_ci_linters/homework/app/main.py
@@ -1,11 +1,15 @@
 import asyncio
+
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.future import select
 from sqlalchemy import asc, desc
+
 from pydantic import ValidationError
+
 from typing import AsyncGenerator
 
 from .models import Base, Table1, Table2
