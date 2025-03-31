612626b0f85bbb6f020309d48c02271deba32568
Simon <135053309+Sim0o0n@users.noreply.github.com>
2025-03-31 14:06:45 +0700

    Update main.py

--- module_30_ci_linters/homework/app/main.py
+++ module_30_ci_linters/homework/app/main.py
@@ -79,7 +79,7 @@ async def get_inf(recipe_id: str, db: AsyncSession = Depends(get_db)):
         )
         recipe_record = result_table1.scalars().first()
         if recipe_record:
-            setattr(recipe_record, "views", recipe_record.views + 1)
+            recipe_record.views += 1  # Обновляем значение счетчика просмотров
             await db.commit()
     except Exception as e:
         print(f"Ошибка при обновлении счетчика просмотров: {e}")
