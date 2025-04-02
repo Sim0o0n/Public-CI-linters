738125ebe0632c3ec229b19299eb64bb6811d83e
Simon <135053309+Sim0o0n@users.noreply.github.com>
2025-04-02 16:37:03 +0700

    Update main.py

--- module_30_ci_linters/homework/app/main.py
+++ module_30_ci_linters/homework/app/main.py
@@ -93,7 +93,7 @@ async def validation_exception_handler(request: Request, exc: ValidationError):
 
 @app.delete("/delete_recipe/{recipe_id}")
 async def delete_recipe(recipe_id: str, db: AsyncSession = db_dependency):
-    result = await db.execute(select(Table2).where(Table2.name_recipe == recipe_id))
+    result = await db.execute(select(Table2).where(Table2.name_recipe == recipe_id))  # noqa: E501
     recipe = result.scalar_one_or_none()
 
     if recipe is None:
@@ -104,7 +104,7 @@ async def delete_recipe(recipe_id: str, db: AsyncSession = db_dependency):
     await db.delete(recipe)
     await db.commit()
 
-    result_table1 = await db.execute(select(Table1).where(Table1.title == recipe_id))
+    result_table1 = await db.execute(select(Table1).where(Table1.title == recipe_id))  # noqa: E501
     recipe_table1 = result_table1.scalar_one_or_none()
 
     if recipe_table1:
